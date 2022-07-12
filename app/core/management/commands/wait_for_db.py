dd4c23e7a0b251aea1fdf378dd23ac1470e45f91
slim allani <slim.allani.sw@gmail.com>
2022-07-12 20:36:24 +0200

    completed test sommand folder

---
+++ app/core/management/commands/wait_for_db.py
@@ -0,0 +1,11 @@
+"""
+Django command to wait for the db to be available.
+"""
+from django.core.management.base import BaseCommand
+
+
+class Command(BaseCommand):
+    """Django command to wait for the db"""
+
+    def handle(self, *args, **options):
+        pass
