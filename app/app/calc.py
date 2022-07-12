51fddcc859bc17e66fa38759cc7e618aa83ff902
slim allani <slim.allani.sw@gmail.com>
2022-07-12 19:24:50 +0200

    fixed linting issues

--- app/app/calc.py
+++ app/app/calc.py
@@ -1,11 +1,13 @@
 """
-Calculator functions 
+Calculator functions
 """
 
-def add(x,y):
+
+def add(x, y):
     """Add x and y"""
     return x + y
 
-def substract(x,y):
+
+def substract(x, y):
     """Asubstract x from y"""
     return y - x
