9dfce809f995fd0e5a1a47cbd4ad669a911a43ba
slim allani <slim.allani.sw@gmail.com>
2022-07-16 20:03:39 +0200

    added auth api

--- app/app/urls.py
+++ app/app/urls.py
@@ -18,7 +18,7 @@ from drf_spectacular.views import (
     SpectacularSwaggerView,
 )
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -26,6 +26,7 @@ urlpatterns = [
     path(
         'api/docs/',
         SpectacularSwaggerView.as_view(url_name='api-schema'),
-        name='api-docs'
-    )
+        name='api-docs',
+    ),
+    path('api/user/', include('user.urls'))
 ]
