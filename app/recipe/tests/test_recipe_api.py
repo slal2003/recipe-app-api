b9571000d8bb16f83c74b024fa1c192ae5f83080
slim allani <slim.allani.sw@gmail.com>
2022-07-21 22:30:56 +0200

    fixed issues

--- app/recipe/tests/test_recipe_api.py
+++ app/recipe/tests/test_recipe_api.py
@@ -84,4 +84,3 @@ class PrivateRecipeApiTests(TestCase):
         serializer = RecipeSerializer(recipes, many=True)
         self.assertEqual(res.status_code, status.HTTP_200_OK)
         self.assertEqual(res.data, serializer.data)
-
